0767fdb3fb3d728544d12440f46958ca37c72c8d
Modeste <136390038+KOUDJENOUME@users.noreply.github.com>
2024-12-02 23:36:11 +0100

    I

--- android/app/src/main/kotlin/com/barbeblanche/tp_eneam_6/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.barbeblanche.gobus
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/dio_instance.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-
-Dio configureDio() {
-  Dio dio = Dio();
-  dio.options.baseUrl = 'http://10.0.2.2:3030/';  // Remplacez par l'URL de votre API
-  dio.options.connectTimeout = Duration(milliseconds: 60000);  // Timeout de connexion
-  dio.options.receiveTimeout = Duration(milliseconds: 60000);  // Timeout de réception
-  return dio;
-}
